1f372f94de50eed66dc44a80cba5fde4a0c06709
Charlie Jones <125832247+CharlieJones2@users.noreply.github.com>
2024-05-12 21:19:55 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 
 songs = [file for file in os.listdir('Songs') if file.endswith('.mp3')]
-# song_titles = [file.split('.')[0].title() for file in os.listdir('Songs') if file.endswith('.mp3')].sort()
+song_titles = [file.split('.')[0].title() for file in os.listdir('Songs') if file.endswith('.mp3')].sort()
 cover = [file for file in os.listdir('Covers') if file.endswith('.png')]
 
 song_and_art = {}
@@ -12,6 +12,7 @@ for song in songs:
 
 def main():
     st.title('Music Player :)')
+    st.write(song_titles)
     
     current_song_index = st.session_state.get('current_song_index', 0)
     
@@ -22,11 +23,6 @@ def main():
     current_song = songs[current_song_index]
     cover_path = 'Covers/cover.png'
     st.image(cover_path, use_column_width=True)
-    
-    if st.button('Previous') and current_song_index > 0:
-        current_song_index -= 1
-    if st.button('Next') and current_song_index < len(songs) - 1:
-        current_song_index += 1
         
     st.session_state.current_song_index = current_song_index
     
