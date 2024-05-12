5d2c235964bd7a9f3208b1d4aacac3c0a78b8d85
Charlie Jones <125832247+CharlieJones2@users.noreply.github.com>
2024-05-12 21:49:38 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,14 +13,17 @@ def main():
     st.title('Music Player :)')
     
     current_song_index = st.session_state.get('current_song_index', 0)
-    st.markdown('![He be dancin](<img src="https://media.tenor.com/yRSnf6wABQ4AAAAi/pato-duck.gif" height="100" width="100" />)')
-    song_selection = st.radio('', songs, index=current_song_index)
+    
+    song_selection = st.radio('scroll for a surprise maybe who knows', songs, index=current_song_index)
     
     st.audio(f'Songs/{song_selection}', format='audio/mp3', start_time=0)
     
     current_song = songs[current_song_index]
     cover_path = 'Covers/cover.png'
     st.image(cover_path, use_column_width=True)
+    
+    st.write('Woah look at him!')
+    st.markdown('![He be dancin](https://media.tenor.com/yRSnf6wABQ4AAAAi/pato-duck.gif)')
         
     st.session_state.current_song_index = current_song_index
     
