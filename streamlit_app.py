5758a1cce7b0e360a0b52c2b5845abeae9a45fc5
Charlie Jones <125832247+CharlieJones2@users.noreply.github.com>
2024-05-12 21:07:53 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,22 +12,17 @@ for song in songs:
 
 def main():
     st.title('Music Player :)')
-    st.sidebar.title('Songs:')
     
-    st.header('All Songs')
     current_song_index = st.session_state.get('current_song_index', 0)
     
-    st.sidebar.title('Playlist')
+    # st.sidebar.title('Playlist')
     song_selection = st.radio('Select Song', songs, index=current_song_index)
     
-    st.sidebar.audio(f'Songs/{song_selection}', format='audio/mp3', start_time=0)
+    st.audio(f'Songs/{song_selection}', format='audio/mp3', start_time=0)
     
     current_song = songs[current_song_index]
-    # cover_art = song_and_art.get(current_song, 'cover.png')
     cover_path = 'Covers/cover.png'
-    st.sidebar.image(cover_path, use_column_width=True)
-    
-    st.success(f'Now Playing: {song_selection}')
+    st.image(cover_path, use_column_width=True)
     
     if st.button('Previous') and current_song_index > 0:
         current_song_index -= 1
